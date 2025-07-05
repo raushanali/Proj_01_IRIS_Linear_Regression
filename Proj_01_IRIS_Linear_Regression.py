4449004f7a83fe37c4c48ad383ed888f6dfcf36a
raushanali <113287161+raushanali@users.noreply.github.com>
2025-07-05 14:22:06 +0530

    Add files via upload

---
+++ Proj_01_IRIS_Linear_Regression.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn import datasets
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+
+irisset = datasets.load_iris()
+
+
+#The Iris dataset was used in R.A. Fisher's classic 1936 paper, 
+#The Use of Multiple Measurements in Taxonomic Problems, 
+#and can also be found on the UCI Machine Learning Repository.
+
+# It includes three iris species with 50 samples each as well as 
+#some properties about each flower.
+# The 3 species of iris are
+#Iris setosa, Iris virginica and Iris versicolor
+
+#The columns in this dataset are:    
+#Id
+#SepalLengthCm
+#SepalWidthCm
+#PetalLengthCm
+#PetalWidthCm
+#Species
+
+# Use only the first 50 samples (Iris setosa)
+X = irisset.data[:50, 0:1]  # Sepal Length
+y = irisset.data[:50, 1]     # Sepal Width
+
+# Fit the linear regression model
+reg = LinearRegression().fit(X, y)
+
+# Predict the values
+yPredict = reg.predict(X)  # Corrected from Predict to predict
+
+# Calculate mean squared error and R^2 score
+mse = mean_squared_error(y, yPredict)
+r2 = r2_score(y, yPredict)
+
+print('Train MSE =', mse)
+print('Train R2 score =', r2)
+print("\n")
+
+
+plt.figure()
+plt.scatter(y, yPredict, color='blue', alpha=0.6)
+plt.plot([y.min(), y.max()], [y.min(), y.max()], color='red', linestyle='--')
+plt.title('Actual vs Predicted Values') 
+plt.xlabel('Actual SepalWidth')
+plt.ylabel('Predicted SepalWidth')
+plt.grid()
+plt.show()
